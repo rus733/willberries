5c0c7b5c5457d9bd7b37b33fdfbfa42384384ac5
rus733 <87997247+rus733@users.noreply.github.com>
2022-02-08 20:08:52 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  // direction: 'vertical',
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.slider-button-next',
+    prevEl: '.slider-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
